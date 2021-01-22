77f6a70171aeb8173afbea44ea490c8581b276cf
Katherine Hansen <katherinehansen@Katherines-MacBook-Air.local>
2021-01-22 11:42:22 -0800

    rearranged

--- final.py
+++ final.py
@@ -8,25 +8,21 @@ from keras import optimizers
 from keras.preprocessing import image
 from keras import models
 from keras import layers
+from keras.applications import VGG16
 
-
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
 
 base_dir = '/Users/katherinehansen/CPSC393/edible-plants/allplants' 
-edibleTrain = os.path.join(base_dir, 'edible_plants/train')
-poisonousTrain = os.path.join(base_dir, 'poisonous_plants/train')
-
-# edibleTest = os.path.join(base_dir, 'edible_plants/test')
-# poisonousTest = os.path.join(base_dir, 'poisonous_plants/test')
-
-test = os.path.join(base_dir, 'test')
-
-edibleValidation = os.path.join(base_dir, 'edible_plants/validation')
-poisonousValidation = os.path.join(base_dir, 'poisonous_plants/validation')
+train_dir = os.path.join(base_dir, 'train')
+test_dir = os.path.join(base_dir, 'test')
+validation_dir = os.path.join(base_dir, 'validation')
 
 
 
 datagen = ImageDataGenerator(rescale=1./255) 
-batch_size = 20
+batch_size = 50
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu',input_shape=(150, 150, 3)))
@@ -44,4 +40,37 @@ model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512)) 
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory, 
+        target_size=(150, 150), 
+        batch_size=batch_size, 
+        class_mode='binary')
+    i=0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch 
+        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+        else:
+            print(i * batch_size , sample_count)
+    return features, labels   
+
+
+# train_features, train_labels = extract_features(train_dir, 6880)
+# print(train_features, train_labels)
+# validation_features, validation_labels = extract_features(validation_dir, 93) 
+# print(validation_features, validation_labels)
+test_features, test_labels = extract_features(test_dir, 416)
+print(test_features, test_labels)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
 
