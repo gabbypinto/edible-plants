c4387cc21ffcff16685fdcf3b047cf9caa822e95
Gabriela Pinto <gabrielapintogp@gmail.com>
2021-01-23 14:57:26 -0800

    got acc

--- final.py
+++ final.py
@@ -8,6 +8,7 @@ from keras import optimizers
 from keras.preprocessing import image
 from keras import models
 from keras import layers
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from keras.applications import VGG16
 
 conv_base = VGG16(weights='imagenet',
@@ -20,26 +21,8 @@ test_dir = os.path.join(base_dir, 'test')
 validation_dir = os.path.join(base_dir, 'validation')
 
 
-
 datagen = ImageDataGenerator(rescale=1./255)
 batch_size = 20
-
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu',input_shape=(150, 150, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.RMSprop(lr=1e-4),
-              metrics=['acc'])
 
 
 def extract_features(directory, sample_count):
@@ -62,57 +45,75 @@ def extract_features(directory, sample_count):
             print(i * batch_size , sample_count)
     return features, labels
 
-
+#feature and labels
 train_features, train_labels = extract_features(train_dir, 6879)
-# print(train_features, train_labels)
-# batch_size = 20
 validation_features, validation_labels = extract_features(validation_dir, 358)
-# print(validation_features, validation_labels)
 test_features, test_labels = extract_features(test_dir, 416)
-# print(test_features, test_labels)
 
+#reshaping
 train_features = np.reshape(train_features, (6879, 4 * 4 * 512))
 validation_features = np.reshape(validation_features, (358, 4 * 4 * 512))
 test_features = np.reshape(test_features, (416, 4 * 4 * 512))
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+
+
 
 model = models.Sequential()
 model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+
+# model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
-loss='binary_crossentropy',
-metrics=['acc'])
+    loss='binary_crossentropy',
+    metrics=['acc'])
 history = model.fit(train_features, train_labels,
-epochs=30,
-batch_size=20,
-validation_data=(validation_features, validation_labels))
+    epochs=30,
+    batch_size=20,
+    validation_data=(validation_features, validation_labels))
 
+print(history.history)
 
-
-
+test_datagen = ImageDataGenerator(rescale=1./255)
+# test_generator = test_datagen.flow_from_directory(
+#     test_dir,
+#     target_size=(150, 150),
+#     batch_size=20,
+#     class_mode='binary')
+# print(test_generator.samples)
+# test_pred = model.predict(test_generator)
+# # test_pred = model.predict(test_features,test_labels)
+# print(test_pred)
+# score = model.evaluate(test_pred, test_labels)
+# # in book
+# # test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
+# print('test acc:', score)
 test_generator = test_datagen.flow_from_directory(
 test_dir,
 target_size=(150, 150),
 batch_size=20,
 class_mode='binary')
-# test_loss, test_acc = model.evaluate(test_generator, steps=50)
-# print('test acc:', test_acc)
-test_acc = model.predict(test_generator)
-print(test_acc)
+
+score = model.evaluate(test_features,test_labels,batch_size=20)
+print('test acc:', score[1])
 
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
+
 epochs = range(1, len(acc) + 1)
+
 plt.plot(epochs, acc, 'bo', label='Training acc')
 plt.plot(epochs, val_acc, 'b', label='Validation acc')
 plt.title('Training and validation accuracy')
 plt.legend()
+
 plt.figure()
+
 plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
