d904a9e878892bf394c25bba344baa2e79a08e4a
Gabriela Pinto <gabrielapintogp@gmail.com>
2021-01-26 14:04:45 -0800

    done

--- final.py
+++ final.py
@@ -19,7 +19,7 @@ import cv2
 
 tf.compat.v1.disable_eager_execution()
 
-#instantia the VGG16 model
+#instantiate the VGG16 model
 conv_base = VGG16(weights='imagenet',
                   include_top=False,
                   input_shape=(150, 150, 3))
@@ -33,9 +33,8 @@ validation_dir = os.path.join(base_dir, 'validation')
 datagen = ImageDataGenerator(rescale=1./255)
 batch_size = 20
 
-pic1pred=""
-pic2pred=""
 
+#extracting features/grabbing features and labels
 def extract_features(directory, sample_count):
     features = np.zeros(shape=(sample_count, 4, 4, 512))
     labels = np.zeros(shape=(sample_count))
@@ -67,15 +66,15 @@ train_features = np.reshape(train_features, (6879, 4 * 4 * 512))
 validation_features = np.reshape(validation_features, (358, 4 * 4 * 512))
 test_features = np.reshape(test_features, (416, 4 * 4 * 512))
 
-
+# constructing our model
 model = models.Sequential()
-# model.add(conv_base)
 model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
 model.add(layers.Dropout(0.2))
 model.add(layers.Dense(32, activation='relu', activity_regularizer=l2(0.001)))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 
+#freezing convolutional base
 conv_base.trainable = True
 set_trainable = False
 for layer in conv_base.layers:
@@ -86,6 +85,7 @@ for layer in conv_base.layers:
     else:
         layer.trainable = False
 
+#compiling and fitting the model
 model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
     loss='binary_crossentropy',
     metrics=['acc'])
@@ -94,37 +94,29 @@ history = model.fit(train_features, train_labels,
     batch_size=20,
     validation_data=(validation_features, validation_labels))
 
-print(history.history)
-
+#test accuracy
 test_datagen = ImageDataGenerator(rescale=1./255)
-
 test_generator = test_datagen.flow_from_directory(
 test_dir,
 target_size=(150, 150),
 batch_size=20,
 class_mode='binary')
-
 score = model.evaluate(test_features,test_labels,batch_size=20)
 print('test acc:', score[1])
 
+#generate heat map
 def heatMapPlot(img_path,final_path):
     img = image.load_img(img_path, target_size=(224, 224))
     model = VGG16(weights='imagenet')
     x = image.img_to_array(img)
-    print(x)
-
     x = np.expand_dims(x, axis=0)
-    print(x.shape)
-
     x = preprocess_input(x)
     preds = model.predict(x)
-    print('Predicted:', decode_predictions(preds, top=3)[0])    #top=2?
-    print(np.argmax(preds[0]))
-    #
-    african_elephant_output = model.output[:, np.argmax(preds[0])]
+
+    pic_output = model.output[:, np.argmax(preds[0])]
     last_conv_layer = model.get_layer('block5_conv3')
 
-    grads = K.gradients(african_elephant_output, last_conv_layer.output)[0]
+    grads = K.gradients(pic_output, last_conv_layer.output)[0]
     pooled_grads = K.mean(grads, axis=(0, 1, 2))
     iterate = K.function([model.input],[pooled_grads, last_conv_layer.output[0]])
     pooled_grads_value, conv_layer_output_value = iterate([x])
@@ -144,25 +136,28 @@ def heatMapPlot(img_path,final_path):
     cv2.imwrite(final_path, superimposed_img)
     plt.clf()
 
-
+#edible plant
 heatMapPlot(r"C:\Users\gabri\Documents\edible-plants\allplants\train\edible_plants\train\Dandellion\510677438_73e4b91c95_m.jpg",r"C:\Users\gabri\Documents\edible-plants\heatmaps\dandelion.jpg")
+#non edible plant
 heatMapPlot(r"C:\Users\gabri\Documents\edible-plants\allplants\train\poisonous_plants\train\lilies\1f340c5e9c379ba637ea6b51476c3d29--lilies-flowers-the-flowers.jpg",r"C:\Users\gabri\Documents\edible-plants\heatmaps\lilies.jpg")
-# acc = history.history['acc']
-# val_acc = history.history['val_acc']
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-#
-# epochs = range(1, len(acc) + 1)
-#
-# plt.plot(epochs, acc, 'bo', label='Training acc')
-# plt.plot(epochs, val_acc, 'b', label='Validation acc')
-# plt.title('Training and validation accuracy')
-# plt.legend()
-#
-# plt.figure()
-#
-# plt.plot(epochs, loss, 'bo', label='Training loss')
-# plt.plot(epochs, val_loss, 'b', label='Validation loss')
-# plt.title('Training and validation loss')
-# plt.legend()
-# plt.show()
+
+#plots - loss and accuracy
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
